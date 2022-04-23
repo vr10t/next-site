a45456b78e54644abc40cb5fad88508e6113305b
vr10t <ai6@tuta.io>
2022-04-23 18:05:36 +0100

    next image

---
+++ pages/terms-conditions.js
@@ -0,0 +1,115 @@
+import Layout from "../src/components/layout";
+export default function Privacy() {
+  return (
+    <Layout>
+      <div className="mx-6">
+        These terms & conditions apply to the booking services (“the Services”)
+        provided by Ismael Taxi whose registered office is at 193-197 Marsh Road, Hemel Hempstead,
+        Hertfordshire, HP3 2QQ as (“the Agent”) as the booking agent for private hire
+        vehicle drivers (“the Drivers”). You acknowledge that the Agent is
+        merely a booking agent for the Drivers and whose only obligation to you
+        is to arrange private hire vehicle transport for you to and from your
+        destination. All calls to our office are recorded for security and
+        training purposes. The Agent shall not be responsible or obliged to
+        refund you for any part of the charges for whatsoever reason. Your
+        contract for payment and transport service is between you and the
+        Driver. If you have more than 2 items of luggage, you must declare any
+        excess items to the Agent at the time of booking but the Agent cannot
+        guarantee that it is able to arrange for a vehicle that would
+        accommodate your entire luggage. Ismael Taxi shall not (without limitation)
+        be held liable for any of the following: (a) any losses whatsoever
+        suffered by you and other fellow passengers as a result of the private
+        hire vehicle arriving late at your pick-up point for whatever reason or
+        any traffic jam or any other matters (whether or not beyond the control
+        of Ismael Taxi) causing a delay in your arrival at your destination. (b) any
+        losses whatsoever suffered by you and other fellow passengers as a
+        result of the private hire vehicle not being able to accommodate all
+        your luggage and other belongings. (c) any damage or loss suffered to
+        your or other passengers belongings (d) any personal injury or death as
+        a result of making this booking. (e) any refusal by the Driver to
+        transport you for whatsoever reason. (f) any delay or inability to
+        perform its obligations under these terms and conditions by any cause
+        beyond its reasonable control including but not restricted to strikes,
+        war, civil disorder and natural disasters. You shall be responsible for
+        the behaviour of all passengers in the vehicle during the whole journey.
+        A charge of £40.00 will be levied to cover the cleaning costs in the
+        unlikely event that the vehicle is soiled by any passenger. Eating,
+        drinking and smoking in the vehicles is not permitted. Exceptional
+        circumstance for babies and medical reasons will be permitted with prior
+        permission. All children should be restrained in a manner that befits
+        the laws and regulations at the time. Suitable child seats should
+        wherever possible be supplied and fitted by the child’s parent /
+        guardian. These can be held by the driver in the event of a return
+        journey booking. The Agent will not carry more passengers than its
+        insurance or licensing allows. The agent is not held responsible for any
+        property left in the vehicle by the passenger. Smoking is never
+        permitted in any of our vehicles. The Agent shall use all reasonable
+        endeavours to get you to your destination on time, but shall not be
+        liable for any loss due to delays caused by road or traffic conditions
+        beyond its control on the journey. Under no circumstances shall The
+        Agent be liable for any loss of profits, business or for any indirect or
+        consequential loss whatever. The Agent aims at running a reliable
+        service for you, however sometimes vehicles cannot arrive at time of
+        booking due to reasons beyond our control. If the vehicle is running
+        late please contact the control immediately for an update. All luggage,
+        belongings, cash, personal property is carried entirely at your own
+        risk. The Agent is entitled to cancel all services and provide up to
+        100% of the refunds (with in 7 Days of the original booking date), in
+        the event of circumstances beyond their control. If a car breaks down
+        during the journey, the Agent will provide an alternative with a
+        practical time allowance. You shall indemnify the Agent against all
+        losses, costs, damages and expenses arising from any act or omission of
+        any passenger in your party. The Agent acts as an agent for clients of
+        self employed drivers, and will pass information relating to the
+        customer in order to assist with the safe and timely pick up. The Agent
+        cannot be held responsible for the misuse of this information by its
+        clients or subsequent drivers that are beyond its control. The Agent may
+        change these terms and conditions at any time by posting changes online.
+        Please review these terms and conditions regularly to ensure that you
+        are aware of any changes. All existing bookings will be at the rate
+        quoted or applicable rate in effect at the time of booking. The Agent
+        reserves the right to change the rules or instructions concerning the
+        use of any part of the service from time to time. The Agent reserves the
+        right to change the service, suspend it temporarily, in the interest of
+        quality of service, communication failure or part interest of other
+        customers. The information, documentation, products and services
+        contained on the Agent website are provided “as is,” without any
+        warranty of any kind. The Agent and its suppliers disclaim all
+        warranties relating to the information, documentation, products and
+        services contained on the Agent website, express or implied, including,
+        but not limited to, any warranties against infringement of third party
+        rights, merchantability and fitness for a particular purpose. The Agent
+        website could include inaccuracies or out-dated information and is
+        subject to change at any time without notice. Your use of this website
+        is at your own risk. You agree to hold the Agent harmless from and not
+        sue the Agent for any claims based upon using this website. In no event
+        shall the Agent be liable for any direct, indirect, incidental, special,
+        or consequential damages, or damages for loss of profits, revenue, data
+        or use, incurred by you or any third party, whether in an action in
+        contract or tort, arising from your access to or use of, this website.
+        On journeys booked Via the App and made using a ‘Card’ payment method,
+        the funds are ‘Pre-Authorised’ when booking to ensure the card is valid
+        and there are enough funds in the account prior to the journey taking
+        place. Wait times & additional fees can be added at will by controllers
+        if there has been additional waiting time over 5 minutes at a stop off
+        or outside the pick up address, or if there is any additional drop offs.
+        If you feel you have been overcharged or you have been wrongly charged
+        please contact us at admin@Ismael TaxiHemel Hempstead.com and we can look into this for
+        you. The above applies for all Card bookings made Via the app, or when
+        bookings have been changed to SMSpay when customers input their Card
+        Details into the SMS link received from Ismael Taxi. Call Recordings Phone
+        calls made to our numbers of 01582 404040 & 01582 494949 may be recorded
+        for training and quality purposes. If any provision of these terms and
+        conditions is held by a court or any competent authority in England to
+        be invalid or unenforceable in whole or in part (the offending
+        provision.) the validity of the other provisions of these conditions and
+        the remainder of the provision in question shall remain in full force
+        and effect as if it had been executed without the offending provision
+        appearing in it. These terms and conditions shall be Ismael Taxiverned by and
+        interpreted in accordance with the law of England and shall be subject
+        to the exclusive jurisdiction of the English courts, to which you and
+        the Agent hereby submit in relation to any dispute thereon.
+      </div>
+    </Layout>
+  );
+}
