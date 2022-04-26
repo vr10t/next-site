74b85ab320126fb00e0d4b4c2bace8bdb0816d34
vr10t <ai6@tuta.io>
2022-04-26 14:31:18 +0100

    fix weird chrome bug

--- src/components/Modal/InputField2.js
+++ src/components/Modal/InputField2.js
@@ -5,7 +5,7 @@ export default function InputField(props) {
      <>
 <label className="flex flex-row text-stone-800 font-bold">{props.icon} {props.label}</label>
         <input
-          className={`flex md:my-2 mx-1  h-16 xl:w-72 ${styles.bgAni} hover:from-orange-500 hover:to-rose-600 bg-gradient-to-r from-orange-400 to-rose-500  rounded-full text-white shadow-md  font-bold placeholder-white md:indent-2 px-4 `}
+          className={`flex md:my-2 mx-1 w-full h-16 xl:w-72 ${styles.bgAni} hover:from-orange-500 hover:to-rose-600 bg-gradient-to-r from-orange-400 to-rose-500  rounded-full text-white shadow-md  font-bold placeholder-white md:indent-2 px-4 `}
           name={props.name}
           required={props.required}
           type={props.type}
